b63129d4e4dceb698cee672009eb02dbb1b33e95
ULADZISLAVA <vladislava96.96@mail.ru>
2023-02-02 16:29:18 +0300

    feat: add prisma to project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import { PrismaClient } from '@prisma/client';
 import express = require('express');
 
 const PORT = process.env.PORT || 8080;
+const client = new PrismaClient();
 
 const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
 
-app.listen(PORT, () => {console.log('server started')})
+app.get('/users/create', async (req, res) => {
+  const user = await client.user.create({
+    data: {
+      email: 'some@mail.ru',
+      password: 'somepass',
+    }
+  })
+
+  res.json(user);
+})
+
+app.listen(PORT, () => {console.log('server started http://localhost:8080')})
