073445c9c165b4992cf5b8890973114e5944fa3a
ULADZISLAVA <vladislava96.96@mail.ru>
2023-02-06 18:39:52 +0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRouter from './routes/userRouter';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', userRouter)
 
